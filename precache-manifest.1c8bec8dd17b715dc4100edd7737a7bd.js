cd0799affadd47b7fe1c2436f145a0e6f31abdba
kirubasankar <45040028+Kirubasankar-Dhnanapal@users.noreply.github.com>
2020-09-01 12:10:03 +0530

    Add files via upload

---
+++ precache-manifest.1c8bec8dd17b715dc4100edd7737a7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e119253a580ff4e4a6416191b22c243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2904d8ed03b162a2fc2",
+    "url": "/static/css/main.7d08b0d6.chunk.css"
+  },
+  {
+    "revision": "cd4084b0fb0f42528744",
+    "url": "/static/js/2.f5d42971.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f5d42971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2904d8ed03b162a2fc2",
+    "url": "/static/js/main.d8c54fba.chunk.js"
+  },
+  {
+    "revision": "ddf9fd89f0e8a46b7abf",
+    "url": "/static/js/runtime-main.b61014f7.js"
+  }
+]);
