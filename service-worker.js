34a6b93bc311bad95bc0822c32fc2053bc361c9b
kirubasankar <45040028+Kirubasankar-Dhnanapal@users.noreply.github.com>
2020-09-01 12:35:28 +0530

    edit commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c8bec8dd17b715dc4100edd7737a7bd.js"
+  "/precache-manifest.52705c1aebf7959c46e4d92a22cac83a.js"
 );
 
 self.addEventListener('message', (event) => {
